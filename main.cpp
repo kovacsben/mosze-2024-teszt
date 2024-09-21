01dfd703d5461ec2a377acfccb38f280a571b3fd
kovacsben <110032413+kovacsben@users.noreply.github.com>
2024-09-21 12:24:05 +0200

    Update main.cpp javítások

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];                          //NELEMENTS helyett N_ELEMENTS
-    std::cout << '1-100 ertekek duplazasa'                //' ' helyett "" és a sor végéről hiányzik a ;
-    for (int i = 0;)                                      // for ciklusból hiányzik i < N_ELEMNTS; i++
+    int *b = new int[N_ELEMENTS];                          //NELEMENTS helyett N_ELEMENTS
+    std::cout << "1-100 ertekek duplazasa";                //' ' helyett "" és a sor végéről hiányzik a ;
+    for (int i = 0; i < N_ELEMENTS; i++)                   // for ciklusból hiányzik i < N_ELEMeNTS; i++
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)                               // for ciklusban i helyett i < N_ELEMENTS;
+    for (int i = 0; i < N_ELEMENTS; i++)                   // for ciklusban i helyett i < N_ELEMENTS;
     {
-        std::cout << "Ertek:"                             // hiányzik a ;
+        std::cout << "Ertek:";                             // hiányzik a ;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)                  // N_ELEMENTS végén , van helyette ; kell
+    for (int i = 0; i < N_ELEMENTS; i++)                   // N_ELEMENTS végén , van helyette ; kell
     {
-        atlag += b[i]                                     // ; hiányzik
+        atlag += b[i];                                     // ; hiányzik
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
